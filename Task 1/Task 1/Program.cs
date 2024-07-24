37d763f90460a1b6979ee139d106dde1804d3518
Ahmed Emad <136331904+ae06520@users.noreply.github.com>
2024-07-24 14:30:59 +0300

    Add Task 1 and 2 code

---
+++ Task 1/Task 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Specialized;
+class Task1
+{
+   public static void Q1()
+    {
+            Console.WriteLine("ENter Numbr TO Check if it is Even or odd");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0)
+                Console.WriteLine("Number is Even", Number);
+            else
+                Console.WriteLine("Number is odd", Number);
+        
+        }
+    public static void Q2()
+    {
+        Console.WriteLine("Enter Your Hight \"in cm\"");
+        
+        int Hight = Convert.ToInt32(Console.ReadLine());
+      
+            Console.WriteLine("Your Hight is", Hight);
+
+    }
+    
+    public static void Q3()
+    {
+        // find largest NO of three No
+        Console.WriteLine("Enter three numbers  ");
+    
+        int No1 = Convert.ToInt32(Console.ReadLine());
+        int No2 = Convert.ToInt32(Console.ReadLine());
+        int No3 = Convert.ToInt32(Console.ReadLine());
+        int largest;
+        if (No1 >= No2 && No1 >= No3) largest = No1;
+        else if (No2 > No1 && No2 > No3) largest = No2;
+        else largest = No3;
+        Console.WriteLine("The Largest number is "+ largest);
+
+    }
+    public static void Main(string[] args)
+    {
+        Q1(); 
+        Q2();
+        Q3();
+
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+class Banking
+{
+    private int id;
+    private string name;
+    private float balance;
+    public Banking()
+    {
+        id = 0;
+        balance = 0f;
+        name = "";
+    }
+    public Banking(int id,float balance,string name)
+    {
+        this.id = id;
+        this.balance = balance;
+        this.name =name;
+    }
+    public void setid(int id)
+    {
+        this.id = id;
+    }
+    public void setname(string name )
+    {
+        this.name = name;
+    }
+    public void setbalance(float balance)
+    {
+        this.balance = balance;
+    }
+    public int getId() 
+    {
+        return id;
+    } 
+    public float getBalance() 
+    { 
+        return balance; 
+    } 
+    public string  getName() 
+    {
+        return name;
+    } 
+    public void Deposite(float amount)
+    {
+        balance += amount;
+        Console.WriteLine("The current acount balance is balance \n", balance);
+    }
+    public void Withdarw(float amount)
+    {
+        balance -= amount;
+        Console.WriteLine("The current acount balance is balance \n", balance);
+    }
+    public void Display()
+    {
+        Console.WriteLine("**************************************************************");
+        Console.WriteLine("Your account Data is:");
+        Console.WriteLine("**************************************************************");
+        Console.WriteLine("Name : name", name);
+        Console.WriteLine("Id: id",id);
+        Console.WriteLine("Balance : balance", balance);
+        Console.WriteLine("**************************************************************");
+
+
+    }
+
+}
+class Task2
+{
+
+}
